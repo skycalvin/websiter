142c813f5e95f11d3af9100d7c92dfe93a07972f
sky <sky.dinata@gmail.com>
2025-01-25 09:45:12 +0700

    Update about_dataframe.py

--- pages/about_dataframe.py
+++ pages/about_dataframe.py
@@ -6,12 +6,15 @@ unique_category = data['category'].unique()
 
 selected_category = st.multiselect("select category",options=unique_category)
 
-criteria1 = data['category'] == 'non food'
+criteria1 = data['category'].isin(selected_category)
+
 criteria2 = data['store_name'] == 'Alfamart'
 criteria3 = data['price'] > 12000
 criteria4 = (data['price'] >= 12000) & (data['price'] <= 40000)
 criteria5 = (criteria1) & (criteria2) & (criteria3)
 
-print(data[criteria5])
+data = data[criteria1]
+
+#print(data[criteria5])
 print(data[criteria5].sort_values('price',ascending=True))
 st.dataframe(data)
