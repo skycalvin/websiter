2aceb95d4cf965af9042e903553db142147975df
sky <sky.dinata@gmail.com>
2025-01-25 09:54:40 +0700

    Update about_dataframe.py

--- pages/about_dataframe.py
+++ pages/about_dataframe.py
@@ -3,14 +3,14 @@ import streamlit as st
 
 data = pd.read_excel('./pages/source.xlsx')
 unique_category = data['category'].unique()
-unique_store_name = data['store_name'].unique()
+unique_store = data['store_name'].unique()
 
 selected_category = st.multiselect("select category",options=unique_category)
-selected_store_name = st.multiselect("select store_name",options=unique_store_name)
+selected_store = st.multiselect("select store",options=unique_store6name)
 
 criteria1 = data['category'].isin(selected_category)
 
-criteria2 = data['store_name'].isin(selected_store_name)
+criteria2 = data['store_name'].isin(selected_store)
 criteria3 = data['price'] > 12000
 criteria4 = (data['price'] >= 12000) & (data['price'] <= 40000)
 criteria5 = (criteria1) & (criteria2) & (criteria3)
