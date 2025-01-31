a49c9719c7a24974836ab7774465ab9486ac49bc
sky <sky.dinata@gmail.com>
2025-01-31 22:28:27 +0700

    Update about_dataframe.py

--- pages/about_dataframe.py
+++ pages/about_dataframe.py
@@ -4,20 +4,22 @@ import streamlit as st
 data = pd.read_excel('./pages/source.xlsx')
 unique_category = data['category'].unique()
 unique_storename = data['store_name'].unique()
+minimum_price = data['price'].min()
+maximum_price = data['price'].max()
 
 selected_category = st.multiselect("select category",options=unique_category)
 selected_store = st.multiselect("select store",options=unique_storename)
+price_point = st.slider("Price",min_value=minimum_price,max_value=maximum_price,value=maximum_price)
 
 criteria1 = data['category'].isin(selected_category)
-
 criteria2 = data['store_name'].isin(selected_store)
-join_criteria = (criteria1) & (criteria2)
+criteria3 = data['price'] <= price_point
+
+join_criteria = (criteria1) & (criteria2) & (criteria3)
 criteria3 = data['price'] > 12000
 criteria4 = (data['price'] >= 12000) & (data['price'] <= 40000)
 criteria5 = (criteria1) & (criteria2) & (criteria3)
 
-data = data[join_criteria]
-
-#print(data[criteria5])
-print(data[criteria5].sort_values('price',ascending=True))
-st.dataframe(data)
+with st.container(border=True):
+  data = data[join_criteria]
+  st.dataframe(data,use_container_width=True)
