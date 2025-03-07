d978d517fb4b942acb6cd7844f6446c6b9770ab3
sky <sky.dinata@gmail.com>
2025-03-07 21:49:39 +0700

    Update converter.py

--- pages/converter.py
+++ pages/converter.py
@@ -13,9 +13,33 @@ conversion_factors = {
             'cabbage':24.6,
             'coke':139,
             'milk':42.3},
-'weignt':None,
-'time':None
-}
+            'instant noodles':340},
+  'weight':{'gr':1/0.001,
+            'kg':1,
+            'ton':1/1000,
+            'adult person':75,
+            'a teenager':60,
+            'cat':3.5,
+            'mouse':0.450,
+            'dog':1.4,
+            'cow':1/1800,
+            'elephant':1/4000,
+            'giraffe':1/1200,
+            'penguin':23,
+            'dolphin':1/4000,
+            'blue whale':1/150000,
+            'brontosaurus':1/22000,
+            't-rex':1/8000,
+  'time':{'seconds':3600,
+          'minutes':60,
+          'hour':1,
+          'day':1/24,
+          'week':1/((24*7)),
+          'month':1/(24*30),
+          'year':1/(24*365),
+          'cat year':1/(24*365*5),
+          'dog year':1/(24*365*5)}
+
 col1,col2.col3,col4,col5 = st.columns(5)
 
 #category selection
